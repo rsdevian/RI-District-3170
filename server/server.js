65c150316bfe8e864ec18c61f9d5be136446ec5d
Vaishnav Nishanth A V <vaishnavnishanth3@gmail.com>
2025-06-22 01:35:26 +0530

    server setup initialized with data fetch

---
+++ server/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import mongoose from "mongoose";
+import { config } from "dotenv";
+import cors from "cors";
+
+config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Task Manager API is running!" });
+});
+
+app.get("/api/hello", (req, res) => {
+    console.log("Hello");
+    res.status(200).json({ message: "Fetched using GET: /api/hello" });
+    return;
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port: http://localhost:${PORT}`);
+});
