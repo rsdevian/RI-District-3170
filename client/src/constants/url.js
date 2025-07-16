6e7ddf01943379162899019525612842b30d1c71
Vaishnav Nishanth A V <rsdevian.devbase@gmail.com>
2025-07-17 01:29:39 +0530

    updated urls

--- client/src/constants/url.js
+++ client/src/constants/url.js
@@ -1,7 +1,7 @@
 const development = false;
 
 const developmentUrl = "http://localhost:3000";
-const productionURL = "https://docshub-dev-v12r.onrender.com";
+const productionURL = "https://docshub-dev-w2ku.onrender.com";
 
 const env = development ? "Development" : "Production";
 const URL = development ? developmentUrl : productionURL;
