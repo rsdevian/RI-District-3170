5341948d29e214be5c0d4051da65b63cd726db22
Vaishnav Nishanth A V <rsdevian.devbase@gmail.com>
2025-07-16 23:12:26 +0530

    updated hosted server url

--- client/src/constants/url.js
+++ client/src/constants/url.js
@@ -1,7 +1,7 @@
-const development = true;
+const development = false;
 
 const developmentUrl = "http://localhost:3000";
-const productionURL = "";
+const productionURL = "https://docshub-dev.onrender.com";
 
 const env = development ? "Development" : "Production";
 const URL = development ? developmentUrl : productionURL;
