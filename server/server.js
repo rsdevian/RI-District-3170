072ef8358ec437a04a8dc3e47be2c2b7764c5bb4
Vaishnav Nishanth A V <rsdevian.devbase@gmail.com>
2025-07-19 22:17:37 +0530

    cors updated

--- server/server.js
+++ server/server.js
@@ -24,6 +24,7 @@ app.use(
             "http://localhost:3001",
             "http://localhost:5173",
             "https://docs-hub-green.vercel.app",
+            "https://rotractdistrict3170.vercel.app",
         ],
         credentials: true,
     })
