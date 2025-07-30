5445acbb74f100fe6f502bc53288371a7a3e9484
Vaishnav Nishanth A V <rsdevian.devbase@gmail.com>
2025-07-30 22:50:04 +0530

    removed sign up button from login form and router

---
+++ client/src/components/NotFound/NotFound.jsx
@@ -0,0 +1,11 @@
+import "../../styles/NotFound.css";
+function NotFound() {
+    return (
+        <div className='not-found--container'>
+            <h1>Page not found</h1>
+            <p>Please contact administrator!</p>
+        </div>
+    );
+}
+
+export default NotFound;
